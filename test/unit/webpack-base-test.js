7bafd9a17c60fa81f4313d16ef51df4a8e8a6b04
lxh961005 <505575121@qq.com>
2020-11-01 00:31:37 +0800

    test: webpack test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig  = require('../../lib/webpack.base.js')
     console.log(baseConfig)
     it('entry', ()=> {
-        assert.equal(baseConfig.entry.index, 'D:/note/webpack/webpack-base/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, 'D:/note/webpack/webpack-base/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index, baseConfig.entry.index.indexOf('webpack-base/test/smoke/template/src/index/index.js')>-1, true)
+        assert.equal(baseConfig.entry.search, baseConfig.entry.search.indexOf('webpack-base/test/smoke/template/src/search/index.js')>-1, true)
     })
 })
